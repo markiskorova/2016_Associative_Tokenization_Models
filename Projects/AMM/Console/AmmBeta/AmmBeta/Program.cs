84187abb854a6526309e0f7a461f78c2d1715d1d
Marc McAllister <129139183+markiskorova@users.noreply.github.com>
2024-07-05 14:22:05 -0700

    Add files via upload

---
+++ Projects/AMM/Console/AmmBeta/AmmBeta/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmmBeta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+        }
+    }
+}
